d7fd7000f337b648fcbb05985a0849626f55c956
Virginie17 <157920288+Virginie17@users.noreply.github.com>
2026-05-21 17:05:01 +0200

    Ajout page volets battants

---
+++ app/volets-battants/page.tsx
@@ -0,0 +1,5 @@
+import { InnerPage } from "../components/InnerPage";
+
+export default function VoletsBattantsPage() {
+  return <InnerPage eyebrow="Volets" title="Volets roulants & battants" text="Pose et rénovation de volets roulants ou battants pour renforcer le confort, l’occultation et la sécurité de votre maison." />;
+}
