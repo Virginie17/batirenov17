86fbd0156f10eed2719b2ecde6a2e73fb65a842a
Virginie17 <157920288+Virginie17@users.noreply.github.com>
2026-05-21 17:08:22 +0200

    Ajout page pergola terrasse

---
+++ app/pergola-terrasse/page.tsx
@@ -0,0 +1,5 @@
+import { InnerPage } from "../components/InnerPage";
+
+export default function PergolaTerrassePage() {
+  return <InnerPage eyebrow="Extérieur" title="Pergola, terrasse & store" text="Aménagements extérieurs pour créer un espace agréable, pratique et esthétique autour de votre maison." />;
+}
