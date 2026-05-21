bbcbef5d6191f669211447465fdfd4bee4a09544
Virginie17 <157920288+Virginie17@users.noreply.github.com>
2026-05-21 17:08:07 +0200

    Ajout page revetement

---
+++ app/revetement/page.tsx
@@ -0,0 +1,5 @@
+import { InnerPage } from "../components/InnerPage";
+
+export default function RevetementPage() {
+  return <InnerPage eyebrow="Revêtements" title="Revêtements sols & murs" text="Pose de revêtements, remise au propre et finitions intérieures pour moderniser vos pièces avec un rendu soigné." />;
+}
