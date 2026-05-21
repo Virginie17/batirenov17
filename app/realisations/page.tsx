b3934e3ecf620a896ad13e61479d818a7775af4c
Virginie17 <157920288+Virginie17@users.noreply.github.com>
2026-05-21 17:04:39 +0200

    Ajout page realisations

---
+++ app/realisations/page.tsx
@@ -0,0 +1,5 @@
+import { InnerPage } from "../components/InnerPage";
+
+export default function RealisationsPage() {
+  return <InnerPage eyebrow="Réalisations" title="Nos réalisations avant / après" text="Découvrez les chantiers réalisés par BâtiReno’V17 : menuiserie, rénovation intérieure, terrasse, portail, salle de bain et aménagement extérieur." />;
+}
