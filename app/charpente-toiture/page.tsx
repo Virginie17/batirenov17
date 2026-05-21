2d25f0d0861e4d2ce7d8d65caf693b6443fa5881
Virginie17 <157920288+Virginie17@users.noreply.github.com>
2026-05-21 17:08:54 +0200

    Ajout page charpente toiture

---
+++ app/charpente-toiture/page.tsx
@@ -0,0 +1,5 @@
+import { InnerPage } from "../components/InnerPage";
+
+export default function CharpenteToiturePage() {
+  return <InnerPage eyebrow="Toiture" title="Charpente, toiture & façade" text="Travaux de charpente, toiture et façade pour préserver, entretenir et valoriser durablement votre maison." />;
+}
