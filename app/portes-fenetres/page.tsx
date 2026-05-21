d422c964353f8e13cb396bdab66bf3713bc23106
Virginie17 <157920288+Virginie17@users.noreply.github.com>
2026-05-21 17:04:51 +0200

    Ajout page portes fenetres

---
+++ app/portes-fenetres/page.tsx
@@ -0,0 +1,5 @@
+import { InnerPage } from "../components/InnerPage";
+
+export default function PortesFenetresPage() {
+  return <InnerPage eyebrow="Menuiseries" title="Portes & fenêtres" text="Pose et remplacement de portes, fenêtres et ouvertures pour améliorer l’isolation, la sécurité et le confort de votre logement." />;
+}
