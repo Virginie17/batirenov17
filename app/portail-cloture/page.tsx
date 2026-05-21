bac386ecf4d0bb1fec2137ececa71a797eafc8d4
Virginie17 <157920288+Virginie17@users.noreply.github.com>
2026-05-21 17:08:41 +0200

    Ajout page portail cloture

---
+++ app/portail-cloture/page.tsx
@@ -0,0 +1,5 @@
+import { InnerPage } from "../components/InnerPage";
+
+export default function PortailCloturePage() {
+  return <InnerPage eyebrow="Extérieur" title="Portail & clôture" text="Installation de portails et clôtures pour sécuriser, délimiter et valoriser votre propriété avec des solutions durables." />;
+}
