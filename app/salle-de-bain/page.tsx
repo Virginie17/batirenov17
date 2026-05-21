604a7f04c94a0ae62cb906cb9bab7a06984af664
Virginie17 <157920288+Virginie17@users.noreply.github.com>
2026-05-21 17:07:53 +0200

    Ajout page salle de bain

---
+++ app/salle-de-bain/page.tsx
@@ -0,0 +1,5 @@
+import { InnerPage } from "../components/InnerPage";
+
+export default function SalleDeBainPage() {
+  return <InnerPage eyebrow="Salle de bain" title="Rénovation de salle de bain" text="Création et rénovation de salle de bain pratique, esthétique et durable, avec des finitions propres et adaptées à votre usage quotidien." />;
+}
