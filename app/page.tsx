0846b151848fc73e37f08f414e6503f2b648fb49
Virginie17 <157920288+Virginie17@users.noreply.github.com>
2026-05-22 09:37:00 +0200

    Integration sections effet wahou homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,10 @@
 import { AboutSection } from "./components/AboutSection";
+import { ArtisanNote } from "./components/ArtisanNote";
+import { CraftGallery } from "./components/CraftGallery";
 import { Hero } from "./components/Hero";
 import { LocalBusinessSchema } from "./components/LocalBusinessSchema";
 import { Portfolio } from "./components/Portfolio";
+import { ProjectSteps } from "./components/ProjectSteps";
 import { ServicesShowcase } from "./components/ServicesShowcase";
 import { SocialProof } from "./components/SocialProof";
 import { ValueCards } from "./components/ValueCards";
@@ -12,10 +15,13 @@ export default function Home() {
     <>
       <LocalBusinessSchema />
       <Hero />
+      <ProjectSteps />
       <AboutSection />
       <ValueCards />
       <ServicesShowcase />
+      <CraftGallery />
       <Portfolio />
+      <ArtisanNote />
       <SocialProof />
       <ZoneIntervention />
     </>
