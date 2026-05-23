68f69c7883bf93596bc4934776f98d3d0c5a08d2
Virginie17 <157920288+Virginie17@users.noreply.github.com>
2026-05-23 09:07:46 +0200

    Simplify homepage section flow

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,10 @@
 import { AboutSection } from "./components/AboutSection";
-import { ArtisanNote } from "./components/ArtisanNote";
-import { CraftGallery } from "./components/CraftGallery";
 import { Hero } from "./components/Hero";
 import { LocalBusinessSchema } from "./components/LocalBusinessSchema";
 import { Portfolio } from "./components/Portfolio";
 import { ProjectSteps } from "./components/ProjectSteps";
 import { ServicesShowcase } from "./components/ServicesShowcase";
-import { SignatureStrip } from "./components/SignatureStrip";
 import { SocialProof } from "./components/SocialProof";
-import { ValueCards } from "./components/ValueCards";
 import { ZoneIntervention } from "./components/ZoneIntervention";
 
 export default function Home() {
@@ -16,14 +12,10 @@ export default function Home() {
     <>
       <LocalBusinessSchema />
       <Hero />
-      <SignatureStrip />
+      <ServicesShowcase />
+      <Portfolio />
       <ProjectSteps />
       <AboutSection />
-      <ValueCards />
-      <ServicesShowcase />
-      <CraftGallery />
-      <Portfolio />
-      <ArtisanNote />
       <SocialProof />
       <ZoneIntervention />
     </>
